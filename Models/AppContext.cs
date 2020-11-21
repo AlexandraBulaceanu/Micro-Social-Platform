80b90d72ec93d545b1c193d49acaad87f2872a03
AlexandraBulaceanu <alemsb29@gmail.com>
2020-11-21 20:58:45 +0200

    Add Comment and Post Controler. Add AppContext. Add Entity Framework

---
+++ Models/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Reaction.Models
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+    }
+}
